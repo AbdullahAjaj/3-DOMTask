4e97479c0fa2506d33fe4e9e4d9c7f9069ac343d
AbdullahAjaj <abdollahmjajaj@gmail.com>
2023-02-19 13:31:26 +0200

    every thing except dialog

---
+++ toDo.js
@@ -0,0 +1,141 @@
+
+// delegateEvent("click",".save",saveBtn);
+let itemsData = [
+    {
+        id: 1676368472841,
+        done: true,
+        color: "#ff0000",
+        title: "To do research",
+        image: "images/image.jpg"
+    },
+    {
+        id: 1676368486003,
+        done: false,
+        color: "#ffa500",
+        title: "Create Prototypes",
+        image: "images/image.jpg"
+    },
+    {
+        id: 1676368496905,
+        done: true,
+        color: "#ffa500",
+        title: "Create design consept",
+        image: "images/image.jpg"
+    },
+    {
+        id: 1676368505544,
+        done: true,
+        color: "#008000",
+        title: "Discuss consept with team",
+        image: "images/image.jpg"
+    },
+]
+
+
+let setDataInLocalStorage = (itemsData) => {
+    localStorage.setItem("myList", JSON.stringify(itemsData))
+}
+
+// setDataInLocalStorage(itemsData)
+let myData = JSON.parse(localStorage.myList)
+
+function createTemplate(item, parentElement){
+    let rootEle = document.getElementsByTagName("section")[0]
+
+    if(parentElement){ // edit
+        let temp = document.getElementById("editOrNew")
+        let clone = temp.content.cloneNode(true)
+
+        clone.querySelector(".color").value = item.color
+        clone.querySelector(".title").value = item.title
+        clone.querySelector(".checkbox").checked = item.done ? "checked" : ""
+        clone.querySelector(".image").src = item.image
+
+        rootEle.replaceChild(clone, parentElement)
+        myData = myData.filter(ele => ele !== item)
+        
+    }else{ // render items
+        let temp = document.getElementById("render")
+        let clone = temp.content.cloneNode(true)
+
+        clone.querySelector(".color").style.backgroundColor = item.color
+        clone.querySelector(".hidden").value = item.id
+        clone.querySelector(".title").innerHTML = item.title
+        clone.querySelector(".checkbox").checked = item.done ? "checked" : ""
+        clone.querySelector(".image").src = item.image
+
+        rootEle.appendChild(clone)
+    }
+}
+
+
+function createStoredElementsUsingLocalStorage(myData){
+    let rootEle = document.getElementsByTagName("section")[0]
+    rootEle.innerHTML = ""
+    myData.forEach(item =>createTemplate(item))
+}
+createStoredElementsUsingLocalStorage(myData)
+
+
+
+
+document.querySelector(".add").addEventListener("click", e => {
+
+    let rootEle = document.getElementsByTagName("section")[0]
+    let temp = document.getElementById("editOrNew")
+    let clone = temp.content.cloneNode(true)
+    rootEle.appendChild(clone)
+    // document.addEventListener("click",{target:".save"},()=>{})
+})
+
+// function delegateEvent(eventName, elementSelector,handler){
+//     document.addEventListener(eventName, (event) => {
+//         if (event.target.closest(elementSelector)) {
+//         handler.call(event.target, event);
+//         }
+//     });
+// }
+
+function saveBtn(e){
+    let parentElement = e.target.parentElement.parentElement
+    
+    let newObj = {}
+    newObj.id = Date.now()
+    newObj.color =  parentElement.children[0].value
+    newObj.title = parentElement.children[2].value
+    newObj.done = parentElement.children[3].checked
+    newObj.image = parentElement.children[5].src
+
+    myData.push(newObj)
+
+    localStorage.setItem("myList", JSON.stringify(myData))
+    location.reload()
+}
+
+function deleteBtn(e){
+    if(confirm("Are you sure want to delete this note?")){
+        if(e?.target?.nextElementSibling?.value){
+            let itemID = e.target.nextElementSibling.value
+            myData = myData.filter(item=> item.id !== +itemID)
+            localStorage.setItem("myList", JSON.stringify(myData))
+        }
+        e.target.parentElement.parentElement.remove()
+    }
+}
+
+
+function filterLists(e, flag){
+    let liList = [...e.target.parentElement.children]
+    liList.forEach(item => item.classList.remove("styled"))
+    e.target.classList.add("styled")
+    let filteredData = myData.filter(item =>item.done === flag)
+    createStoredElementsUsingLocalStorage(filteredData)
+}
+
+function editBtn(e){
+    let parentElement = e.target.parentElement.parentElement
+    let myID = parentElement.children[1].children[2].value
+    let myItem = myData.filter(ele=>ele.id === +myID)
+    createTemplate(myItem[0], parentElement)
+  
+}
